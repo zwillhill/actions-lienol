f4eb78ef97d0f4f16923cd7cf6372ea82312af84
Hanz <zwillhill@hotmail.com>
2021-07-10 16:31:11 +0800

    Create stablegit.sh

---
+++ k3/stablegit.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# Lienol K3 Stable Git.sh
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+############################
+
+# Replace k3screenctrl
+rm -rf package/lean/k3screenctrl
+git clone https://github.com/lwz322/k3screenctrl_build.git package/lwz322/k3screenctrl
+# Add luci-app-k3screenctrl
+git clone https://github.com/lwz322/luci-app-k3screenctrl.git package/lwz322/luci-app-k3screenctrl
+
+# Replace luci-app-dnsfilter
+#rm -rf package/lean/luci-app-dnsfilter
+git clone https://github.com/garypang13/luci-app-dnsfilter.git package/garypang13/luci-app-dnsfilter
+
+# Add luci-theme-darkmatter with moded one
+svn co https://github.com/zwillhill/luci-theme-darkmatter/trunk/luci/themes/luci-theme-darkmatter package/apollo-ng/luci-theme-darkmatter
+
+# Add luci-app-bypass
+git clone https://github.com/garypang13/openwrt-bypass.git package/garypang13/openwrt-bypass
+
+# Add bypass depends from xiaorouji
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/chinadns-ng package/xiaorouji/chinadns-ng
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-go package/xiaorouji/trojan-go
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-plus package/xiaorouji/trojan-plus
+# Add bypass depends from fw876
+svn co https://github.com/fw876/helloworld/trunk/naiveproxy package/fw876/naiveproxy
+svn co https://github.com/fw876/helloworld/trunk/shadowsocksr-libev package/fw876/shadowsocksr-libev
+svn co https://github.com/fw876/helloworld/trunk/tcping package/fw876/tcping
+svn co https://github.com/fw876/helloworld/trunk/v2ray-core package/fw876/v2ray-core
+svn co https://github.com/fw876/helloworld/trunk/v2ray-plugin package/fw876/v2ray-plugin
+svn co https://github.com/fw876/helloworld/trunk/xray-core package/fw876/xray-core
+svn co https://github.com/fw876/helloworld/trunk/xray-plugin package/fw876/xray-plugin
